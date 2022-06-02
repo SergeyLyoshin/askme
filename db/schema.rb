bfd7208010dccf1f15897f22d67654b268c0f642
slyoshin <slyoshin@icloud.com>
2022-06-02 14:58:40 +0500

    Add Validation and Authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_27_084752) do
+ActiveRecord::Schema.define(version: 2022_06_02_065123) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
     t.string "answer"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -24,6 +26,11 @@ ActiveRecord::Schema.define(version: 2022_05_27_084752) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "avatar_url"
+    t.string "email"
+    t.string "password_hash"
+    t.string "password_salt"
   end
 
+  add_foreign_key "questions", "users"
 end
