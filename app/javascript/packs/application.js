bccb9de996a4dbe00d6dfb7a05fc7274718c33cd
slyoshin <slyoshin@icloud.com>
2022-06-13 12:45:07 +0500

    49 lesson JS form not worked

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,18 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery3
+//= require jquery_ujs
+//= require_tree
+
 import Rails from "@rails/ujs"
 
 Rails.start()
+
+$(function(){
+    $('#ask-button').click(function(){
+      $('#ask-form').slideToggle(300);
+      return false;
+    });
+  });
+
